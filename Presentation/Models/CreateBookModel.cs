0ed0142856e4a13b7398fc71032ed32aee01808a
fenea <fenea95@gmail.com>
2018-01-11 01:59:54 +0200

    added book upload

--- Presentation/Models/CreateBookModel.cs
+++ Presentation/Models/CreateBookModel.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace Presentation.Models
 {
@@ -11,11 +15,11 @@ namespace Presentation.Models
 
         public string Type { get; set; }
 
-        public string Path { get; set; }
+        public IFormFile BookFile { get; set; }
 
         public string Author { get; set; }
 
-        public string PhotoPath { get; set; }
+        public IFormFile PhotoPath { get; set; }
 
         public string Description { get; set; }
 
